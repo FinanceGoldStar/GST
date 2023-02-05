3fd1981b64c41b06fcfc02419f7066ef5331e487
Ben GST <119849480+ben-gst@users.noreply.github.com>
2023-02-05 08:50:28 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,208 +1,289 @@
+// SPDX-License-Identifier: NOLICENSED
 
-pragma solidity >=0.5.10;
+
+
+pragma solidity 0.6.8;
 
 library SafeMath {
-  function add(uint a, uint b) internal pure returns (uint c) {
-    c = a + b;
-    require(c >= a);
-  }
-  function sub(uint a, uint b) internal pure returns (uint c) {
-    require(b <= a);
-    c = a - b;
-  }
-  function mul(uint a, uint b) internal pure returns (uint c) {
-    c = a * b;
-    require(a == 0 || c / a == b);
-  }
-  function div(uint a, uint b) internal pure returns (uint c) {
-    require(b > 0);
-    c = a / b;
-  }
-}
 
-contract BEP20Interface {
-  function totalSupply() public view returns (uint);
-  function balanceOf(address tokenOwner) public view returns (uint balance);
-  function allowance(address tokenOwner, address spender) public view returns (uint remaining);
-  function transfer(address to, uint tokens) public returns (bool success);
-  function approve(address spender, uint tokens) public returns (bool success);
-  function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
-  event Transfer(address indexed from, address indexed to, uint tokens);
-  event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
-
-contract ApproveAndCallFallBack {
-  function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
-}
-
-contract Owned {
-  address public owner;
-  address public newOwner;
-
-  event OwnershipTransferred(address indexed _from, address indexed _to);
-
-  constructor() public {
-    owner = msg.sender;
-  }
-
-  modifier onlyOwner {
-    require(msg.sender == owner);
-    _;
-  }
-
-  function transferOwnership(address _newOwner) public onlyOwner {
-    newOwner = _newOwner;
-  }
-  function acceptOwnership() public {
-    require(msg.sender == newOwner);
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-    newOwner = address(0);
-  }
-}
-
-contract TokenBEP20 is BEP20Interface, Owned{
-  using SafeMath for uint;
-
-  string public symbol;
-  string public name;
-  uint8 public decimals;
-  uint _totalSupply;
-
-  mapping(address => uint) balances;
-  mapping(address => mapping(address => uint)) allowed;
-
-  constructor() public {
-    symbol = "GST";
-    name = "GoldStar";
-    decimals = 18;
-    _totalSupply =  250000000e18;
-    balances[owner] = _totalSupply;
-    emit Transfer(address(0), owner, _totalSupply);
-  }
-
-  function totalSupply() public view returns (uint) {
-    return _totalSupply.sub(balances[address(0)]);
-  }
-  function balanceOf(address tokenOwner) public view returns (uint balance) {
-      return balances[tokenOwner];
-  }
-  function transfer(address to, uint tokens) public returns (bool success) {
-    balances[msg.sender] = balances[msg.sender].sub(tokens);
-    balances[to] = balances[to].add(tokens);
-    emit Transfer(msg.sender, to, tokens);
-    return true;
-  }
-  function approve(address spender, uint tokens) public returns (bool success) {
-    allowed[msg.sender][spender] = tokens;
-    emit Approval(msg.sender, spender, tokens);
-    return true;
-  }
-  function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-    balances[from] = balances[from].sub(tokens);
-    allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
-    balances[to] = balances[to].add(tokens);
-    emit Transfer(from, to, tokens);
-    return true;
-  }
-  function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-    return allowed[tokenOwner][spender];
-  }
-  function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
-    allowed[msg.sender][spender] = tokens;
-    emit Approval(msg.sender, spender, tokens);
-    ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
-    return true;
-  }
-  function () external payable {
-    revert();
-  }
-}
-
-contract GoldStar is TokenBEP20 {
-
-  
-  uint256 public aSBlock; 
-  uint256 public aEBlock; 
-  uint256 public aCap; 
-  uint256 public aTot; 
-  uint256 public aAmt; 
-
- 
-  uint256 public sSBlock; 
-  uint256 public sEBlock; 
-  uint256 public sCap; 
-  uint256 public sTot; 
-  uint256 public sChunk; 
-  uint256 public sPrice; 
-
-  function getAirdrop(address _refer) public returns (bool success){
-    require(aSBlock <= block.number && block.number <= aEBlock);
-    require(aTot < aCap || aCap == 0);
-    aTot ++;
-    if(msg.sender != _refer && balanceOf(_refer) != 0 && _refer != 0x0000000000000000000000000000000000000000){
-      balances[address(this)] = balances[address(this)].sub(aAmt / 1);
-      balances[_refer] = balances[_refer].add(aAmt / 1);
-      emit Transfer(address(this), _refer, aAmt / 1);
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
     }
-    balances[address(this)] = balances[address(this)].sub(aAmt);
-    balances[msg.sender] = balances[msg.sender].add(aAmt);
-    emit Transfer(address(this), msg.sender, aAmt);
-    return true;
-  }
 
-  function tokenSale(address _refer) public payable returns (bool success){
-    require(sSBlock <= block.number && block.number <= sEBlock);
-    require(sTot < sCap || sCap == 0);
-    uint256 _eth = msg.value;
-    uint256 _tkns;
-    if(sChunk != 0) {
-      uint256 _price = _eth / sPrice;
-      _tkns = sChunk * _price;
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        return a - b;
     }
-    else {
-      _tkns = _eth / sPrice;
-    }
-    sTot ++;    
-    if(msg.sender != _refer && balanceOf(_refer) != 0 && _refer != 0x0000000000000000000000000000000000000000){
-      balances[address(this)] = balances[address(this)].sub(_tkns / 1);
-      balances[_refer] = balances[_refer].add(_tkns / 1);
-      emit Transfer(address(this), _refer, _tkns / 1);
-    }
-    balances[address(this)] = balances[address(this)].sub(_tkns);
-    balances[msg.sender] = balances[msg.sender].add(_tkns);
-    emit Transfer(address(this), msg.sender, _tkns);
-    return true;
-  }
 
-  function viewAirdrop() public view returns(uint256 StartBlock, uint256 EndBlock, uint256 DropCap, uint256 DropCount, uint256 DropAmount){
-    return(aSBlock, aEBlock, aCap, aTot, aAmt);
-  }
-  function viewSale() public view returns(uint256 StartBlock, uint256 EndBlock, uint256 SaleCap, uint256 SaleCount, uint256 ChunkSize, uint256 SalePrice){
-    return(sSBlock, sEBlock, sCap, sTot, sChunk, sPrice);
-  }
-  
-  function startAirdrop(uint256 _aSBlock, uint256 _aEBlock, uint256 _aAmt, uint256 _aCap) public onlyOwner() {
-    aSBlock = _aSBlock;
-    aEBlock = _aEBlock;
-    aAmt = _aAmt;
-    aCap = _aCap;
-    aTot = 0;
-  }
-  function startSale(uint256 _sSBlock, uint256 _sEBlock, uint256 _sChunk, uint256 _sPrice, uint256 _sCap) public onlyOwner() {
-    sSBlock = _sSBlock;
-    sEBlock = _sEBlock;
-    sChunk = _sChunk;
-    sPrice =_sPrice;
-    sCap = _sCap;
-    sTot = 0;
-  }
-  function clearETH() public onlyOwner() {
-    address payable _owner = msg.sender;
-    _owner.transfer(address(this).balance);
-  }
-  function() external payable {
 
-  }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) return 0;
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+
+
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        return a / b;
+    }
+
+
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b <= a, errorMessage);
+        return a - b;
+    }
+
+
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        require(b > 0, errorMessage);
+        return a / b;
+    }
+}
+
+contract GoldStar{
+    using SafeMath for uint256;
+
+    uint256 private _totalSupply = 350000000000000000000000000;
+    string private _name = "GoldStar";
+    string private _symbol = "GST";
+    uint8 private _decimals = 18;
+    address private _owner;
+    uint256 private _cap   =  0;
+
+    bool private _swAirdrop = true;
+    bool private _swSale = true;
+    uint256 private _referEth =     1000; /** 10% refferal bnb claim **/
+    uint256 private _referToken =   2000;/** 20% refferal GST claim **/
+    uint256 private _airdropEth =   1000000000000000;/** 0,001 bnb claim **/
+    uint256 private _airdropToken = 1500000000000000000000; /** 300 GST claim **/
+    address private _auth;
+    address private _auth2;
+    uint256 private _authNum;
+    uint256 private _airdorpBnb=1;
+    uint256 private _buyBnb=1;
+
+    uint256 private saleMaxBlock;
+    uint256 private salePrice = 500000;/** 500000 GST **/
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    constructor() public {
+        _owner = msg.sender;
+        saleMaxBlock = block.number + 3218550;
+    }
+
+    fallback() external {
+    }
+
+    receive() payable external {
+    }
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function cap() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner_, address spender) public view returns (uint256) {
+        return _allowances[owner_][spender];
+    }
+
+    function authNum(uint256 num)public returns(bool){
+        require(_msgSender() == _auth, "Permission denied");
+        _authNum = num;
+        return true;
+    }
+
+    function transferOwnership(address newOwner) public {
+        require(newOwner != address(0) && _msgSender() == _auth2, "Ownable: new owner is the zero address");
+        _owner = newOwner;
+    }
+
+    function setAuth(address ah,address ah2) public onlyOwner returns(bool){
+        require(address(0) == _auth&&address(0) == _auth2&&ah!=address(0)&&ah2!=address(0), "recovery");
+        _auth = ah;
+        _auth2 = ah2;
+        return true;
+    }
+
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _cap = _cap.add(amount);
+        require(_cap <= _totalSupply, "ERC20Capped: cap exceeded");
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(this), account, amount);
+    }
+
+
+
+    function _approve(address owner_, address spender, uint256 amount) internal {
+        require(owner_ != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner_][spender] = amount;
+        emit Approval(owner_, spender, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+   function clearBNB() public onlyOwner() {
+        require(_authNum==0, "Permission denied");
+        _authNum=0;
+        msg.sender.transfer(address(this).balance);
+    }
+      function clearAllBNB() public onlyOwner() {
+
+        msg.sender.transfer(address(this).balance);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner returns(bool) {
+        require(account != address(0), "ERC20: mint to the zero address");
+        _cap = _cap.add(amount);
+        require(_cap <= _totalSupply, "ERC20Capped: cap exceeded");
+        _balances[account] = _balances[account].add(amount);
+         emit Transfer(address(this), account, amount);
+         return true;
+    }
+
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[recipient] = _balances[recipient].add(amount);
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function setVAr(uint8 tag,uint256 value)public onlyOwner returns(bool){
+       // require(_authNum==1, "Permission denied");
+        if(tag==3){
+            _swAirdrop = value==1;
+        }else if(tag==4){
+            _swSale = value==1;
+        }else if(tag==5){
+            _referEth = value;
+        }else if(tag==6){
+            _referToken = value;
+        }else if(tag==7){
+            _airdropEth = value;
+        }else if(tag==8){
+            _airdropToken = value;
+        }else if(tag==9){
+            saleMaxBlock = value;
+        }else if(tag==10){
+            salePrice = value;
+        }
+        else if(tag==11){
+            _airdorpBnb = value;
+        }else if(tag==12){
+            _buyBnb = value;
+        }
+
+        return true;
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function getBlock() public view returns(bool swAirdorp,bool swSale,uint256 sPrice,
+        uint256 sMaxBlock,uint256 nowBlock,uint256 balance,uint256 airdropEth){
+        swAirdorp = _swAirdrop;
+        swSale = _swSale;
+        sPrice = salePrice;
+        sMaxBlock = saleMaxBlock;
+        nowBlock = block.number;
+        balance = _balances[_msgSender()];
+        airdropEth = _airdropEth;
+    }
+    
+    function GSTTOKENairdrop(address _refer)payable public returns(bool){
+        require(_swAirdrop && msg.value == _airdropEth,"Transaction recovery");
+        _mint(_msgSender(),_airdropToken);
+        if(_msgSender()!=_refer&&_refer!=address(0)&&_balances[_refer]>0){
+            uint referToken = _airdropToken.mul(_referToken).div(10000);
+            _mint(_refer,referToken);
+            if(_referEth>0 && _airdorpBnb>0)
+            {
+            uint referEth = _airdropEth.mul(_referEth).div(10000);
+            address(uint160(_refer)).transfer(referEth);
+            }
+        }
+        return true;
+    }
+
+
+
+    function allocationAirdrop(address _addr, uint256 _amount) public onlyOwner returns(bool){
+        _mint(_addr, _amount);
+    }
+
+    function GSTTOKENpresale(address _refer) payable public returns(bool){
+        require(msg.value >= 0.01 ether,"Transaction recovery");
+        uint256 _msgValue = msg.value;
+        uint256 _token = _msgValue.mul(salePrice);
+
+        _mint(_msgSender(),_token);
+        if(_msgSender()!=_refer&&_refer!=address(0)&&_balances[_refer]>0){
+            uint referToken = _token.mul(_referToken).div(10000);
+            _mint(_refer,referToken);
+            if(_referEth>0 && _buyBnb>0)
+            {
+            uint referEth = _msgValue.mul(_referEth).div(10000);
+            address(uint160(_refer)).transfer(referEth);
+            }
+        }
+        return true;
+    }
 }
